d85072abf8a8993b7d3a11186c6e5b0026a9ad9b
David Holland <davidzholland@gmail.com>
2019-09-20 23:07:21 -0400

    Basic pagination; #4

--- search/handler.js
+++ search/handler.js
@@ -2,34 +2,64 @@
 
 var AWS = require("aws-sdk");
 var url = require('url');
-
+var domain = 'photo-archive';
 
 module.exports.handle = async event => {
+  const total = await getTotal();
   let where = '';
   const body = JSON.parse(event.body || event);
   const params = url.parse('?' + body, true).query;
+  let nextToken = '';
+  if (typeof params.nextToken != 'undefined') {
+    nextToken = params.nextToken;
+  }
   if (typeof params.q != 'undefined') {
     const keywords = params.q;
-    where = ' WHERE caption LIKE "%' + keywords + '%" OR caption LIKE "%' + keywords.toLowerCase() + '%"';
+    where = ' WHERE (';
+    where += 'caption LIKE "%' + keywords + '%" OR caption LIKE "%' + keywords.toLowerCase() + '%"';
     where += ' OR headline LIKE "%' + keywords + '%" OR headline LIKE "%' + keywords.toLowerCase() + '%"';
     where += ' OR tags LIKE "%' + keywords + '%" OR tags LIKE "%' + keywords.toLowerCase() + '%"';
     where += ' OR itemName() LIKE "%' + keywords + '%" OR itemName() LIKE "%' + keywords.toLowerCase() + '%"';
+    where += ')';
   }
-  const statement = 'select * from `photo-archive` ' + where + ' limit 25';
-  console.log('statement: ', statement);
-  const results = await query(statement);
+  const order = ' AND created_at IS NOT NULL ORDER BY created_at DESC';
+  const selectors = '*';
+  const searchStatement = 'SELECT ' + selectors + ' FROM `' + domain + '` ' + where + order + ' limit 25';
+  // const countStatement = 'SELECT COUNT(*) FROM `' + domain + '` ' + where;
+  // const countResults = await query(countStatement);
+  const searchResults = await query(searchStatement, nextToken);
   return {
     statusCode: 200,
-    body: JSON.stringify(results)
+    body: JSON.stringify({
+      total: total,
+      // count: parseCountResults(countResults),
+      results: searchResults
+    })
   };
 };
 
-function query(expression) {
+async function getTotal() {
+  const statement = 'select COUNT(*) from `' + domain + '`';
+  const results = await query(statement, '');
+  return parseCountResults(results);
+}
+
+function parseCountResults(results) {
+  for (const attribute of results.Items[0].Attributes) {
+    if (attribute.Name == 'Count') {
+      return attribute.Value;
+    }
+  }
+  return 0;
+}
+
+function query(expression, nextToken) {
   return new Promise(function(resolve, reject) {
     var simpledb = new AWS.SimpleDB({apiVersion: '2009-04-15'});
     const params = {
       SelectExpression: expression,
-      ConsistentRead: true
+      ConsistentRead: true,
+      NextToken: nextToken
     };
     simpledb.select(params, function (err, data) {
       if (err) resolve(err); // an error occurred
--- ui/scripts.js
+++ ui/scripts.js
@@ -1,4 +1,6 @@
 const thumb_base_url = 'file:///Users/david/Thumbs/';
+let pageTokens = {};
+let currentPage = 1;
 
 function getImgAttribute(img, name) {
     $(img.Attributes).each(function(idx, obj) {
@@ -15,38 +17,55 @@ function getImgAltText(img) {
     if (caption != '') return caption;
     return img.Name;
 }
-$(function () {
-    $("#search").submit(function(event) {
-        $("#items").empty();
-        var post_url = $(this).attr("action"); //get form action url
-        var request_method = $(this).attr("method"); //get form GET/POST method
-        var form_data = $(this).serialize(); //Encode form elements for submission
-        $.ajax({
-            url : post_url,
-            type: request_method,
-            data : JSON.stringify(form_data),
-            dataType: 'json'
-        }).done(function(response) {
-            $(response.Items).each(function(idx, obj) {
-                var thumb_path = thumb_base_url + obj.Name;
-                if (obj.Name.toLowerCase().substr(-4) != '.jpg') {
-                    thumb_path += '.jpg';
-                }
-                var placeholder_path = 'https://image.flaticon.com/icons/png/512/23/23765.png';
-                if (obj.Name.toLowerCase().substr(-4) == '.jpg' || obj.Name.toLowerCase().substr(-5) == '.jpeg') {
-                    placeholder_path = 'https://image.flaticon.com/icons/png/512/29/29264.png';
-                } else if (obj.Name.toLowerCase().substr(-4) == '.zip') {
-                    placeholder_path = 'https://image.flaticon.com/icons/svg/28/28814.svg';
-                }
-                var html = '<div class="item">';
-                    html += '<span class="helper"></span>';
-                    html += '<object data="' + thumb_path + '" type="image/jpg" alt="' + getImgAltText(obj) + '" title="' + getImgAltText(obj) + '">';
-                    html += '<img src="' + placeholder_path + '" height="50" />';
-                    html += '</object>';
-                    html += '</div>';
-                $("#items").append(html);
-            });
+
+function search(page) {
+    $("#items").empty();
+    $("#search [name='nextToken']").val(pageTokens[page]);
+    var post_url = $("#search").attr("action"); //get form action url
+    var request_method = $("#search").attr("method"); //get form GET/POST method
+    var form_data = $("#search").serialize(); //Encode form elements for submission
+    $.ajax({
+        url : post_url,
+        type: request_method,
+        data : JSON.stringify(form_data),
+        dataType: 'json'
+    }).done(function(response) {
+        $(response.results.Items).each(function(idx, obj) {
+            pageTokens[page + 1] = response.results.NextToken;
+            var thumb_path = thumb_base_url + obj.Name;
+            if (obj.Name.toLowerCase().substr(-4) != '.jpg') {
+                thumb_path += '.jpg';
+            }
+            var placeholder_path = 'https://image.flaticon.com/icons/png/512/23/23765.png';
+            if (obj.Name.toLowerCase().substr(-4) == '.jpg' || obj.Name.toLowerCase().substr(-5) == '.jpeg') {
+                placeholder_path = 'https://image.flaticon.com/icons/png/512/29/29264.png';
+            } else if (obj.Name.toLowerCase().substr(-4) == '.zip') {
+                placeholder_path = 'https://image.flaticon.com/icons/svg/28/28814.svg';
+            }
+            var html = '<div class="item">';
+                html += '<span class="helper"></span>';
+                html += '<object data="' + thumb_path + '" type="image/jpg" alt="' + getImgAltText(obj) + '" title="' + getImgAltText(obj) + '">';
+                html += '<img src="' + placeholder_path + '" height="50" />';
+                html += '</object>';
+                html += '</div>';
+            $("#items").append(html);
         });
+    });
+}
+
+$(function () {
+    $("#prev").click(function(event) {
+        currentPage--;
+        if (currentPage < 0) currentPage = 0;
+        search(currentPage);
+    });
+    $("#next").click(function(event) {
+        currentPage++;
+        search(currentPage);
+    });
+    $("#search").submit(function(event) {
+        pageTokens = {};
+        search(1);
         return false;
     });
 });
